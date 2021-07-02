d8f1045e3ea4b8c99a2c89d7be82d8e1fbd0dec7
dorustsitera <dorustsitera@gmail.com>
2021-07-02 02:07:39 +0100

    changement de code vers pointeur

--- main.c
+++ main.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
-
+int somme(int a, int b);
 int main(void)
 {
-	int code ;
-
-    printf("Bonjour\n");
-
-    printf("Entrer votre code\n");
-    do{
-
-
-    scanf("%d", &code);
-    printf("code = %d \n", code );
-} while (code != 0); 
+	int a = 2;
+	int b = 6;
+	printf("Saisir de nomnbres\n");
+	scanf("%d%d",&a, &b);
+	printf("La somme de a et b est %d\n", somme(a, b) );
 	return 0;
+}
+int somme(int a , int b)
+{
+	int s = a + b;
+	return s;
 }
